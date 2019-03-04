cab8800798a9999d303cc4f4cffdf4c04dfa0a66
Aleksander Sil <olek130@gmail.com>
2019-03-04 15:27:26 +0100

    Moved data conversion to function

--- lab21.py
+++ lab21.py
@@ -8,6 +8,8 @@ import json
 import matplotlib.pyplot as plt
 import sys
 
+import numpy as np
+
 from lab5 import world_map
 
 scan = []
@@ -42,51 +44,35 @@ def saveJSON(filename, data):
     with open(filename, 'w') as outfile:
         json.dump(data, outfile)
 
-
-
-
-def lab21():
-    global scan, robot, theta, data
-    data = readJSON(inputPath + "map_boxes_0.json")
-
+def convertDataToGlobalCoordinates(data):
     x = np.arange(0, 512)
     theta = (np.pi / 512) * x
 
     pointsHit = list()
 
     for iteration in data:
-        print iteration['pose']
-
         pointsHitTemp = list()
         for scanIndex in range(len(iteration['scan'])):
-            scanPoint = [ theta[scanIndex], iteration['scan'][scanIndex] ]
-
-            if not np.isinf(scanPoint[1]) and not np.isnan(scanPoint[1]):
-                pointsHitTemp.append(pol2cart(scanPoint[0], scanPoint[1]))
-
+            if not np.isinf(iteration['scan'][scanIndex]) and not np.isnan(iteration['scan'][scanIndex]):
+                scanPointX = iteration['pose'][0] + 0.18 * math.cos(iteration['pose'][2]) + iteration['scan'][
+                    scanIndex] * math.cos(iteration['pose'][2] + theta[scanIndex])
+                scanPointY = iteration['pose'][1] + 0.18 * math.sin(iteration['pose'][2]) + iteration['scan'][
+                    scanIndex] * math.sin(iteration['pose'][2] + theta[scanIndex])
+                pointsHitTemp.append([scanPointX, scanPointY])
 
         pointsHit.append(pointsHitTemp)
-
-    saveJSON(outputPath + "temp.JSON", pointsHit)
-
-    print "Lenght of sublists in each iteration: "
-    print len(pointsHit[0])
-    print len(pointsHit[1])
-    print len(pointsHit[2])
-    print len(pointsHit[3])
-
-    # scan=robot.ReadLaser()
-
-# if the cell size is small then we need to consider that the sensor is a bit to the front of the robot.
-
+    return pointsHit
 
 
 
 if __name__ == '__main__':
 	wm=world_map(7,7,0.5)
-	for row in wm.initialize_map():
-		print row
-	
-	for row in wm.update_map(0,0,1.0):
-		print row
-    	lab21()
+    for row in wm.initialize_map():
+        print row
+
+    for row in wm.update_map(0,0,1.0):
+        print row
+
+    data = readJSON(inputPath + "map_boxes_0.json")
+    pointsHit = convertDataToGlobalCoordinates(data)
+    saveJSON(outputPath + "temp.JSON", pointsHit)
