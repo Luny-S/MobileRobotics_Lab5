c7cee1d3a57205547f01288ec71d729f7e2e9a63
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2019-04-29 13:09:18 +0200

    Robot control skeleton

---
+++ RobotControl.py
@@ -0,0 +1,26 @@
+
+# 0. Enlarge obstacles by robot radius (/)
+# 1. Check robot position in cell cooordinates
+# 2. Plan path (StartPoint = RobotPosition, GoalPoint = any) (/)
+# 3. Rotate from robot orientation to first segment direction
+# 4. Move forward until direction changes
+# 5. Rotate from orientation to next segment direction
+# 6. Move forward
+# 7. Repeat until goal reached or the world ends
+# *** Optimise path to turn while driving
+
+from map import world_map
+import PathPlanning
+
+probabilityThreshold = 0.75
+robotRadius = 0.1
+
+def ControlRobot(probabilityMap, goalPoint, robotPosition):
+
+    PathPlanning.enlargeObstacles(probabilityMap)
+    #startPoint = getPose, poseToCell
+
+	WaveMap = PathPlanning.blastWave(probabilityMap, startPoint, goalPoint)
+	PathPoints = PathPlanning.findPathPoints(WaveMap, startPoint, goalPoint)
+
+    #3,4,5,6,7,...
